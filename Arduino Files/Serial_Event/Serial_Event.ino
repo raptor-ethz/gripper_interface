c8431a7f2e05434a746c83f8dcafe0c85601555c
aurelappius <appiusa@student.ethz.ch>
2021-12-22 14:02:56 +0100

    debug #3

--- Arduino Files/Serial_Event/Serial_Event.ino
+++ Arduino Files/Serial_Event/Serial_Event.ino
@@ -14,7 +14,7 @@ void setup() {
   l_servo.attach(6);
 
   // close fully
-  l_servo.write(5);
+  l_servo.write(25);
   r_servo.write(175);
 }
 
@@ -22,13 +22,13 @@ void loop() {}
 void serialEvent() {
   while (Serial.available()) {
     int cmd = (int)Serial.read();
-    if (cmd >= 0 && cmd < max_angle) {  // left arm
+    if ((cmd >= 0) && (cmd < max_angle)) {  // left arm
       l_servo.write(cmd);
     }
-    if (cmd >= max_angle && cmd < 2 * max_angle) {  // right arm
+    if ((cmd >= max_angle) && (cmd < 2 * max_angle)) {  // right arm
       r_servo.write(180 - (cmd - max_angle));
     }
-    if (cmd >= 2 * max_angle && cmd < 3 * max_angle) {  // both arms
+    if ((cmd >= 2 * max_angle) && (cmd < 3 * max_angle)) {  // both arms
       l_servo.write(cmd - 2 * max_angle);
       r_servo.write(180 - (cmd - 2 * max_angle));
     }
