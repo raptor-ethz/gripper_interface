5f632791e2e3b0e2a9e9c99088f16893b5412eb9
aurel <appiusa@student.ethz.ch>
2022-03-24 16:46:27 +0100

    updated to new comm_lib architecture

--- app/include/sub_callback.h
+++ app/include/sub_callback.h
@@ -1,8 +1,8 @@
 #pragma once
-#include "QuadPositionCmdPubSubTypes.h"
-#include "quadcopter_msgs/msgs/QuadPositionCmd.h"
+#include "RotGripCmd_msg.h"
+#include "RotGripCmd_msgPubSubTypes.h"
 
 // Subscriber data that needs to be accessed in main
 namespace sub {
-cpp_msg::QuadPositionCmd grip_cmd;
-}  // namespace sub
+cpp_msg::RotGripCmd_msg grip_cmd;
+} // namespace sub
